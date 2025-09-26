c390249f4b19d37a7022a678f3ba7bfe9653cfda
Brett <brettdansmith@gmail.com>
2025-09-26 14:42:17 -0400

    Update TODOs in renderFib. Planning for display

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import { renderForm } from "./ui/renderForm";
-
+import { renderFib } from "./ui/renderFib";
 renderForm(onSubmit);
 
-function onSubmit(alg) {
+async function onSubmit(alg) {
     console.log(alg + ":");
     if(alg == "fib") {
+        await renderFib();
         console.log("-------is fib");
     } else if(alg =="merge") {
         console.log("-------is merge");
---
+++ src/ui/renderFib.js
@@ -0,0 +1,13 @@
+
+export async function renderFib() {
+
+}
+
+async function fibs(n) {
+    // iterative version:
+    //TODO: each time throught the for loop, display in three separate cards across the screen:
+    //      1. value i
+    //      2. updated fibs array
+    //      3. current line of code running?
+    //      have this all sync up with the code running maybe?
+}
